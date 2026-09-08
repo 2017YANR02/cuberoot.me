65d2d76b66f541a2bffc8cbddf29004e170ce33d
CubeRoot <46429560+2017YANR02@users.noreply.github.com>
2026-09-08 04:22:17 -0700

    refactor: move Douyin transcript exporter to standalone repository

--- scripts/douyin-transcript/start.ps1
+++
@@ -1,7 +0,0 @@
-param([string]$Url)
-$ErrorActionPreference = 'Stop'
-[Console]::OutputEncoding = [Text.Encoding]::UTF8
-if (-not (Get-Command node -ErrorAction SilentlyContinue)) { throw '请先安装 Node.js 22 或更新的 LTS 版本：https://nodejs.org/en/download/' }
-$entry = Join-Path $PSScriptRoot 'start.mjs'
-if ($Url) { & node $entry $Url } else { & node $entry }
-exit $LASTEXITCODE
